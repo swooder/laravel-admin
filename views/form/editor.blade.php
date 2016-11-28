eeee37346a9ac68d149d34865365880f52e23782
shaojie <shaojie@octinn.com>
2016-11-28 22:56:15 +0800

    add ueditor

--- views/form/editor.blade.php
+++ views/form/editor.blade.php
@@ -1,3 +1,4 @@
+@include('UEditor::head')
 <div class="form-group {!! !$errors->has($label) ?: 'has-error' !!}">
 
     <label for="{{$id}}" class="col-sm-2 control-label">{{$label}}</label>
@@ -6,6 +7,17 @@
 
         @include('admin::form.error')
 
-        <textarea class="form-control" id="{{$id}}" name="{{$name}}" placeholder="{{ trans('admin::lang.input') }} {{$label}}" {!! $attributes !!} >{{ old($column, $value) }}</textarea>
+        {{--<textarea class="form-control" id="{{$id}}" name="{{$name}}" placeholder="{{ trans('admin::lang.input') }} {{$label}}" {!! $attributes !!} >{{ old($column, $value) }}</textarea>--}}
+        <script id="container" id="{{$id}}" name="{{$name}}" type="text/plain">
+            {{$value}}
+        </script>
+
+        <script type="text/javascript">
+            var ue = UE.getEditor('container');
+            ue.ready(function() {
+                ue.execCommand('serverparam', '_token', '{{ csrf_token() }}');
+            });
+        </script>
+
     </div>
 </div>
