26cbc36ff0c36c396bfa44eaac51604edcfa0443
shaojie <shaojie@octinn.com>
2016-11-28 22:32:40 +0800

    add ueditor

--- src/Form/Field/Editor.php
+++ src/Form/Field/Editor.php
@@ -6,14 +6,5 @@ use Encore\Admin\Form\Field;
 
 class Editor extends Field
 {
-    protected static $js = [
-        '//cdn.ckeditor.com/4.5.10/standard/ckeditor.js',
-    ];
 
-    public function render()
-    {
-        $this->script = "CKEDITOR.replace('{$this->column}');";
-
-        return parent::render();
-    }
 }
